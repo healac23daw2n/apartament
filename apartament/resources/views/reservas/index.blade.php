a20088d20ca1f6ae751def78d74acb7a348e1bcc
healac23daw2 <060097.clot@fje.edu>
2024-05-16 11:47:51 +0200

    creación de los apartados de la tabla reservas

--- apartament/resources/views/reservas/index.blade.php
+++ apartament/resources/views/reservas/index.blade.php
@@ -25,6 +25,65 @@
                     <th>Tipo de aseguranza</th>
                 </tr>
             </thead>
+
+            <tbody>
+                <tr>
+                <td>12345678A</td>
+                <td>AP102</td>
+                <td>2024-05-20</td>
+                <td>14:00</td>
+                <td>2024-05-25</td>
+                <td>Recepción</td>
+                <td>$100</td>
+                <td>Si</td>
+                <td>Franquicia hasta 1000 Euros</td>
+                    <td>
+                        <a href="#" class="btn btn-primary btn-sm mb-2">Editar</a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>87654321B</td>
+                    <td>AP205</td>
+                    <td>2024-06-10</td>
+                    <td>12:00</td>
+                    <td>2024-06-20</td>
+                    <td>Oficina del administrador</td>
+                    <td>$120</td>
+                    <td>Sí</td>
+                    <td>Sin franquicia</td>
+                    <td>
+                        <a href="#" class="btn btn-primary btn-sm mb-2">Editar</a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>98765432C</td>
+                    <td>AP306</td>
+                    <td>2024-07-05</td>
+                    <td>15:00</td>
+                    <td>2024-07-15</td>
+                    <td>Recepción principal</td>
+                    <td>$90</td>
+                    <td>No</td>
+                    <td>Franquicia hasta 500 Euros</td>
+                    <td>
+                        <a href="#" class="btn btn-primary btn-sm mb-2">Editar</a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>23456789D</td>
+                    <td>AP401</td>
+                    <td>2024-08-15</td>
+                    <td>13:30</td>
+                    <td>2024-08-25</td>
+                    <td>Oficina de servicio al cliente</td>
+                    <td>$150</td>
+                    <td>Sí</td>
+                    <td>Franquicia hasta 1000 Euros</td>
+                    <td>
+                        <a href="#" class="btn btn-primary btn-sm mb-2">Editar</a>
+                    </td>
+                </tr>
+            </tbody>
         </table>
     </div>
 @stop
