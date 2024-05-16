881d01b432bc24a55e2f55461175c6f0434a58fb
healac23daw2 <060097.clot@fje.edu>
2024-05-16 14:36:53 +0200

    modificación del return de rules de ClientsRequest.php

--- apartament/app/Http/Requests/ClientsRequest.php
+++ apartament/app/Http/Requests/ClientsRequest.php
@@ -22,7 +22,7 @@ class ClientsRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => 'required | string' 
         ];
     }
 }
